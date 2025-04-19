c52a1de81f8e09783d42a6dfe232c6fe54a1fb55
KatieGrays <katiegray@ufl.edu>
2025-04-19 19:36:26 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from lab11 import *
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
